dda77d67deff12192037aee2a725f327fb21259f
madasuramanji <ramanjimadasu24@gmail.com>
2025-05-05 12:05:19 +0530

    MiniProject of DispalyData

---
+++ projectpilot/src/App.jsx
@@ -0,0 +1,10 @@
+import Projectspage from "./projects/Projectspage";
+function App() {
+  return (
+    <div className="container">
+         <Projectspage/>
+      </div>
+  );
+}
+
+export default App;
